651c296abee0141b09eac1fc2795485145343822
AGC79 <gudafreecam@gmail.com>
2025-12-18 12:42:20 +0100

    repo actualizado

--- 3-Machine_Learning/3-Deep_Learning/1-Intro_Deep_Learning/ejercicios/prueba/main.py
+++
@@ -1,9 +0,0 @@
-import funciones as f
-import variables as v
-
-df = f.leer(v.path)
-f.graficos(df)
-X, y = f.x_y(df, v.target)
-X_train, X_test, y_train, y_test = f.split_X_y(X, y, v.tamano_test, v.semilla)
-X_train_s, X_test_s = f.escalado(X_train, X_test, v.escalado)
-print(X_train_s)
